a3da0a95977d8efe06941fbdc9e49dbeb3e920a2
Taha Khapra <tkhapra@hof-university.de>
2025-10-19 01:08:36 +0200

    Added a window for the board and logic for switching players

--- Config.py
+++ Config.py
@@ -2,5 +2,14 @@ import json
 from pathlib import Path
 
 class Config:
-    WIDTH = 800
-    HEIGHT = 600
+    # const values
+    width = 720
+    height = 720
+    rows = 3
+    cols = 3
+    size = width//rows
+    lineWidth = 15
+
+    # colors
+    bgColor = (255, 255, 255)
+    lineColor = (0, 0, 0)
--- main.py
+++ main.py
@@ -1,7 +1,67 @@
 import sys
 import pygame
+import numpy as np
+
 from Config import Config
 
+#ref: https://www.youtube.com/watch?v=Bk9hlNZc6sE&t=343s
 pygame.init()
-screen = pygame.display.set_mode((Config.WIDTH, Config.HEIGHT))
+screen = pygame.display.set_mode((Config.width, Config.height))
 pygame.display.set_caption('Ultimate Tic Tac Toe')
+screen.fill(Config.bgColor)
+
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((Config.rows, Config.cols))
+
+        #debug -> initial state
+        print(self.squares)
+
+    def mark_square(self, row, col, player):
+        self.squares[row][col] = player
+
+    def if_empty_square(self, row, col):
+        return self.squares[row][col] == 0
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.player = 1
+        self.show_lines()
+
+    def show_lines(self):
+        pygame.draw.line(screen, Config.lineColor, (Config.size, 0), (Config.size, Config.height), Config.lineWidth)
+        pygame.draw.line(screen, Config.lineColor, (Config.width-Config.size, 0), (Config.width-Config.size, Config.height), Config.lineWidth)
+
+        pygame.draw.line(screen, Config.lineColor, (0, Config.size), (Config.width, Config.size), Config.lineWidth)
+        pygame.draw.line(screen, Config.lineColor, (0, Config.height-Config.size), (Config.width, Config.height-Config.size), Config.lineWidth)
+
+    def switch_player(self):
+        self.player = self.player % 2 + 1
+
+def main():
+    game = Game()
+    board = game.board
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+
+                #x-axis
+                col = pos[0] // Config.size
+                #y-axis
+                row = pos[1] // Config.size
+
+                if board.if_empty_square(row, col):
+                    board.mark_square(row, col, game.player)
+                    game.switch_player()
+                    print(board.squares)
+
+        pygame.display.update()
+
+main()
