971f6c550a3c08394a88a76f109f06a4cd09e7ae
Taha Khapra <tkhapra@hof-university.de>
2025-10-21 00:49:21 +0200

    added hover logic along with some ultimate logic
    ultimate logic still needs some refining

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#todo: dirty rects might need to be implemented to clear the local board and render the winner of a square
+
 import sys
 import pygame
 import numpy as np
@@ -13,6 +15,7 @@ screen.fill(BG_COLOR)
 class Board:
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
+        #self.squares = np.zeros((ROWS, COLS, ROWS, COLS))
         self.empty_squares = self.squares
         self.marked_squares = 0
 
@@ -23,20 +26,26 @@ class Board:
         self.squares[row][col] = player
         self.marked_squares += 1
 
+    """def mark_square(self, global_row, global_col, local_row, local_col, player):
+        self.squares[global_row][global_col][local_row][local_col] = player
+
+    def is_local_square_empty(self, global_row, global_col, local_row, local_col):
+        return self.squares[global_row][global_col][local_row][local_col] == 0"""
+
     def is_board_full(self):
         return self.marked_squares == 9
 
     def is_board_empty(self):
         return self.marked_squares == 0
 
-    def if_empty_square(self, row, col):
+    def is_empty_square(self, row, col):
         return self.squares[row][col] == 0
 
     def get_empty_squares(self):
         empty_squares = []
         for row in range(ROWS):
             for col in range(COLS):
-                if self.if_empty_square(row, col):
+                if self.is_empty_square(row, col):
                     empty_squares.append((row, col))
 
         return empty_squares
@@ -46,6 +55,8 @@ class Game:
         self.board = Board()
         self.player = 1
         self.show_lines()
+        self.hover = None
+        self.allowed_square = None
 
     def show_lines(self):
 
@@ -60,7 +71,7 @@ class Game:
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT-SIZE),
                          (WIDTH, HEIGHT-SIZE), LINE_WIDTH)
 
-        #logic is missing so commenting it out
+        #logic is still sort of missing so commenting it out
         """#local boards
         for row in range(ROWS):
             for col in range(COLS):
@@ -87,6 +98,29 @@ class Game:
                                  (board_x_offset + SIZE, board_y_offset + 2*LINE_SIZE),
                                  LOCAL_LINE_WIDTH)"""
 
+    #need to clear the whole board for the hover ui so everything needs to be redrawn
+    def draw_all_again(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                player = self.board.squares[row][col]
+                if player != 0:
+                    original_player = self.player
+                    self.player = int(player)
+                    self.draw_fig(row, col)
+                    self.player = original_player
+
+    """def draw_all_again(self):
+        for global_row in range(ROWS):
+            for global_col in range(COLS):
+                for local_row in range(ROWS):
+                    for local_col in range(COLS):
+                        player = self.board.squares[global_row][global_col][local_row][local_col]
+                        if player != 0:
+                            original_player = self.player
+                            self.player = int(player)
+                            self.draw_fig(global_row, global_col, local_row, local_col)
+                            self.player = original_player"""
+
     def draw_fig(self, row, col):
         #X-shape
         if self.player == 1:
@@ -104,6 +138,78 @@ class Game:
             center = (col*SIZE + SIZE//2, row*SIZE + SIZE//2)
             pygame.draw.circle(screen, LINE_COLOR, center, RADIUS, LINE_WIDTH)
 
+    """def draw_fig(self, global_row, global_col, local_row, local_col):
+        local_center_x = global_col * SIZE + local_col * LINE_SIZE + LINE_SIZE // 2
+        local_center_y = global_row * SIZE + local_row * LINE_SIZE + LINE_SIZE // 2
+        padding = LINE_SIZE * 0.25
+
+        # X-shape
+        if self.player == 1:
+            x1 = local_center_x - LINE_SIZE // 2 + padding
+            x2 = local_center_x + LINE_SIZE // 2 - padding
+            y1 = local_center_y - LINE_SIZE // 2 + padding
+            y2 = local_center_y + LINE_SIZE // 2 - padding
+            local_width = LOCAL_LINE_WIDTH + 2
+
+            # \-shape
+            pygame.draw.line(screen, LINE_COLOR, (x1, y1), (x2, y2), local_width)
+            # /-shape
+            pygame.draw.line(screen, LINE_COLOR, (x1, y2), (x2, y1), local_width)
+
+        # O-shape
+        elif self.player == 2:
+            pygame.draw.circle(screen, LINE_COLOR, (local_center_x, local_center_y),
+                               LOCAL_RADIUS, LOCAL_LINE_WIDTH)"""
+
+    def get_hovered_square(self, mouse_pos):
+        mouse_x, mouse_y = mouse_pos
+        global_col = mouse_x // SIZE
+        global_row = mouse_y // SIZE
+
+        #stay between the defined parameters
+        if not (0 <= global_row < ROWS and 0 <= global_col < COLS):
+            return None
+
+        return global_row, global_col
+
+    def draw_allowed_square(self):
+        if self.allowed_square is None:
+            return
+
+        global_row, global_col = self.allowed_square
+        x = global_col * SIZE
+        y = global_row * SIZE
+
+        surface = pygame.Surface((SIZE, SIZE), pygame.SRCALPHA)
+        surface.fill(ALLOWED_SQUARE_COLOR)
+        screen.blit(surface, (x, y))
+
+    """def is_move_legal(self, global_row, global_col, local_row, local_col):
+        if not self.board.is_local_square_empty(global_row, global_col, local_row, local_col):
+            return False
+
+        if self.allowed_square is None:
+            return True
+        else:
+            allowed_global_row, allowed_global_col = self.allowed_square
+            return (global_row, global_col) == (allowed_global_row, allowed_global_col)"""
+
+    def draw_hover(self):
+        if self.hover is None:
+            return
+
+        global_row, global_col = self.hover
+
+        hover_dim = SIZE
+
+        x = (global_col * SIZE)
+        y = (global_row * SIZE)
+
+        surface = pygame.Surface((hover_dim, hover_dim), pygame.SRCALPHA)
+        surface.fill(HOVER_COLOR)
+
+        screen.blit(surface, (x, y))
+
     def switch_player(self):
         self.player = self.player % 2 + 1
 
@@ -112,25 +218,40 @@ def main():
     board = game.board
 
     while True:
+        screen.fill(BG_COLOR)
+        game.show_lines()
+        game.draw_all_again()
+        game.draw_allowed_square()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEMOTION:
                 pos = event.pos
+                game.hover = game.get_hovered_square(pos)
 
-                #x-axis
-                col = pos[0] // SIZE
-                #y-axis
-                row = pos[1] // SIZE
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if game.hover is not None:
+                    row, col = game.hover
+                    mouse_x, mouse_y = event.pos
+                    local_col = (mouse_x % SIZE) // LINE_SIZE
+                    local_row = (mouse_y % SIZE) // LINE_SIZE
 
-                if board.if_empty_square(row, col):
-                    board.mark_square(row, col, game.player)
-                    game.draw_fig(row,col)
-                    game.switch_player()
-                    print(board.squares)
+                    if board.is_empty_square(row, col):
+                        board.mark_square(row, col, game.player)
+                        game.draw_fig(row,col)
+                        game.switch_player()
+                        print(board.squares)
 
+                    """if game.is_move_legal(global_row, global_col, local_row, local_col):
+                        board.mark_square(global_row, global_col, local_row, local_col, game.player)
+                        game.allowed_square = (local_row, local_col)
+                        game.switch_player()
+                        print(board.squares)"""
+
+        game.draw_hover()
         pygame.display.update()
 
 main()
